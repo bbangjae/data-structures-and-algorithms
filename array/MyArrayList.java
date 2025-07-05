2bb10e74481d5b238940e3d8c5d9fe2582de2cb9
bbangjae <thebyunjae@naver.com>
2025-07-05 19:58:54 +0900

    feat(array): MyArrayList 클래스 및 테스트 코드 추가

---
+++ array/MyArrayList.java
@@ -0,0 +1,111 @@
+package array;
+
+import java.util.Arrays;
+
+public class MyArrayList<E> {
+
+    private static final int DEFAULT_CAPACITY = 5;
+    private Object[] elements;
+    private int size;
+
+    public MyArrayList() {
+        this.elements = new Object[DEFAULT_CAPACITY];
+        this.size = 0;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public void add(E item) {
+        if (this.size == this.elements.length) {
+            ensureCapacity();
+        }
+
+        this.elements[this.size] = item;
+        this.size++;
+    }
+
+    public void add(int index, E item) {
+        checkPositionIndex(index, true);
+
+        if (this.size == this.elements.length) {
+            ensureCapacity();
+        }
+
+        shiftRight(index);
+        this.elements[index] = item;
+        this.size++;
+    }
+
+    @SuppressWarnings("unchecked")
+    public E get(int index) {
+        return (E) this.elements[index];
+    }
+
+    void remove(int index) {
+        checkPositionIndex(index, false);
+
+        this.size--;
+        shiftLeft(index);
+        this.elements[this.size] = null;
+    }
+
+
+    public void set(E item, int index) {
+        checkPositionIndex(index, false);
+        this.elements[index] = item;
+    }
+
+    public int indexOf(E item) {
+        for (int i = 0; i < this.size; i++) {
+            if (this.elements[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean contains(E item) {
+        for (int i = 0; i < this.size; i++) {
+            if (this.elements[i].equals(item)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "MyArrayList{" +
+                "elements = " + Arrays.toString(this.elements) +
+                ", size = " + this.size +
+                '}';
+    }
+
+    private void ensureCapacity() {
+        if (this.size == this.elements.length) {
+            this.elements = Arrays.copyOf(this.elements, this.elements.length * 2);
+        }
+    }
+
+    private void shiftLeft(int index) {
+        for (int i = index; i < this.size; i++) {
+            this.elements[i] = this.elements[i + 1];
+        }
+    }
+
+    private void shiftRight(int index) {
+        for (int i = this.size; i > index; i--) {
+            this.elements[i] = this.elements[i - 1];
+        }
+    }
+
+    private void checkPositionIndex (int index, boolean allowEnd) {
+        int upper = allowEnd ? this.size : this.size - 1;
+
+        if (index < 0 || index > upper) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + this.size);
+        }
+    }
+}
---
+++ array/MyArrayListTest.java
@@ -0,0 +1,48 @@
+package array;
+
+
+public class MyArrayListTest {
+    public static void main(String[] args) {
+
+        MyArrayList<Integer> myArrayList = new MyArrayList<>();
+
+        myArrayList.add(0);
+        System.out.println("0을 추가한 후: " + myArrayList);
+
+        myArrayList.add(1);
+        System.out.println("1을 추가한 후: " + myArrayList);
+
+        myArrayList.add(2);
+        System.out.println("2를 추가한 후: " + myArrayList);
+
+        myArrayList.add(3);
+        System.out.println("3을 추가한 후: " + myArrayList);
+
+        myArrayList.add(4);
+        System.out.println("4를 추가한 후: " + myArrayList);
+
+        myArrayList.remove(4);
+        System.out.println("인덱스 4의 값을 삭제한 후: " + myArrayList);
+
+        myArrayList.add(5);
+        System.out.println("5를 추가한 후: " + myArrayList);
+
+        myArrayList.add(4, 4);
+        System.out.println("인덱스 4에 4을 삽입한 후: " + myArrayList);
+
+        myArrayList.remove(5);
+        System.out.println("인덱스 5의 값을 삭제한 후: " + myArrayList);
+
+        myArrayList.remove(2);
+        System.out.println("인덱스 2의 값을 삭제한 후: " + myArrayList);
+
+        myArrayList.set(2, 2);
+        System.out.println("인덱스 2의 값을 2로 변경한 후: " + myArrayList);
+
+        System.out.println("값 2의 인덱스: " + myArrayList.indexOf(2));
+        System.out.println("값 2의 포함여부: " + myArrayList.contains(2));
+
+        System.out.println("값 5의 인덱스: " + myArrayList.indexOf(5));
+        System.out.println("값 5의 포함여부: " + myArrayList.contains(5));
+    }
+}
